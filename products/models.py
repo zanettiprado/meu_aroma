e8298ed85913bfc9951a6f4b2adb0e62456a51f2
zanettiprado <zanettiprado@gmail.com>
2023-12-19 21:12:10 +0000

    Create first model to added the first product

---
+++ products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return self.name
+    
+
