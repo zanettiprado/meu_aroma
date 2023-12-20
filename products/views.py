5cd569dc8075411cbc69c9a243eab5023b3b4bdb
zanettiprado <zanettiprado@gmail.com>
2023-12-20 23:10:41 +0000

    Search query funcionality added by name or description

--- products/views.py
+++ products/views.py
@@ -1,17 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
 
 def all_products(request):
-    
     products = Product.objects.all()
-    
+    query = None 
+
+    if request.GET.get('q'):
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "Please enter a search term to find products.")
+            return redirect(reverse('products'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query) # returning search result for description or name
+        products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query,
     }
-    
-    return render(request, 'products/products.html', context) 
+
+    return render(request, 'products/products.html', context)
+
 
 
 def product_detail(request, product_id):
