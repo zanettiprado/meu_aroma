dfe2fe5bafe9c0f16f6613ca33dcb4cbe2aa616c
zanettiprado <zanettiprado@gmail.com>
2023-12-21 00:28:50 +0000

    Added functionality to sort products by category

--- products/views.py
+++ products/views.py
@@ -1,31 +1,35 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
-
-# Create your views here.
+from .models import Product, Category
 
 def all_products(request):
     products = Product.objects.all()
-    query = None 
+    query = None
+    categories = None
 
-    if request.GET.get('q'):
-        query = request.GET['q']
-        if not query:
-            messages.error(request, "Please enter a search term to find products.")
-            return redirect(reverse('products'))
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-        queries = Q(name__icontains=query) | Q(description__icontains=query) # returning search result for description or name
-        products = products.filter(queries)
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a search term to find products.")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
-
-
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
