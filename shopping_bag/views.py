759db8333882bbd6d8abfb77a9f16e91a6ac2f3a
zanettiprado <zanettiprado@gmail.com>
2023-12-22 17:08:39 +0000

    Add form and first view to haddle prodcuts in the bag

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product 
 # Create your views here.
 
 def view_bag(request):
     
     return render(request, 'shopping_bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+       
+    # product = get_object_or_404(Product, pk=item_id)  
+    quantity = int(request.POST.get('quantity'))  
+    redirect_url = request.POST.get('redirect_url')
+
+    bag = request.session.get('bag', {})  
+    
+    if item_id in bag:
+        bag[item_id] += quantity  
+    else:
+        bag[item_id] = quantity  
+
+    request.session['bag'] = bag
+    
+    print(request.session['bag'])  
+    # messages.success(request, f'Added {product.name} to your bag, keep buying')
+
+    return redirect(redirect_url)
