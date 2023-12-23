0943ba14cd780bf890233a3415f6083e1a46e04f
zanettiprado <zanettiprado@gmail.com>
2023-12-23 18:24:52 +0000

    create context.py for bag app

--- meu_aroma/settings.py
+++ meu_aroma/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shopping_bag.context.bag_contents'
             ],
         },
     },
@@ -156,3 +157,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 45
+STANDARD_DELIVERY_PERCENTAGE = 8
