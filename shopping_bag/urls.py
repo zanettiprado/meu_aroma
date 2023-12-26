e907d061f6c555a85847f35f400c517635ef7703
zanettiprado <zanettiprado@gmail.com>
2023-12-26 18:17:14 +0000

    function to add product to the bag straight from the products page

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
     path('add/<int:item_id>/', views.add_to_bag, name='add_to_bag'),
     path('remove/<item_id>/', remove_from_bag, name='shopping_bag_remove'),
+    path('add_to_bag/<int:product_id>/', views.add_to_bag, name='add_to_bag'),
 
 ]
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -2,20 +2,23 @@ from django.shortcuts import render, redirect
 
 
 def view_bag(request):
-
     return render(request, 'shopping_bag/bag.html')
 
-def add_to_bag(request, item_id):
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+def add_to_bag(request, product_id):
+    if request.method == 'POST':
+        
+        quantity = int(request.POST.get('quantity', 1))
+        redirect_url = request.POST.get('redirect_url')
+        bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+        if str(product_id) in bag:
+            bag[str(product_id)] += quantity
+        else:
+            bag[str(product_id)] = quantity
+
+        request.session['bag'] = bag
+        return redirect(redirect_url)
     else:
-        bag[item_id] = quantity
-
-    request.session['bag'] = bag
-    return redirect(redirect_url)
-    
+        
+        return redirect('products')
