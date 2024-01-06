9c55dc37966fa4978bf5eda9c0f0c21635f795d7
zanettiprado <zanettiprado@gmail.com>
2024-01-06 22:37:29 +0000

    change image fixing it on edit and add

--- products/forms.py
+++ products/forms.py
@@ -1,16 +1,21 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product 
 
 class QuantityForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=99, label='Quantity')
 
 
-class ProductForm(forms.ModelForm):
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)    
 
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-
+        widgets = {
+            'image': CustomClearableFileInput(attrs={'class': 'form-control custom-class another-class'}),
+        }
+        
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
