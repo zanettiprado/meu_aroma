f4785db8d2ba0f9d326b1d88af4e648140f39668
zanettiprado <zanettiprado@gmail.com>
2024-01-06 20:18:36 +0000

    update profile user information in checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from shopping_bag.context import bag_contents
 
 import stripe 
@@ -49,6 +51,23 @@ def checkout(request):
             messages.error(request, "Failed to create a payment intent: " + str(e))
             return redirect(reverse('checkout'))
         
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'phone_number': profile.default_phone_number,
+                    'town_or_city': profile.default_town_or_city,
+                    'county': profile.default_county,
+                    'postcode': profile.default_postcode,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
         if not stripe_public_key:
             messages.warning(request, 'It appears the Stripe public key is missing. Please confirm it has been set.')
 
@@ -70,12 +89,32 @@ def checkout(request):
     return render(request, 'checkout/checkout.html', context)
 
 def checkout_success(request, order_number):
- 
+
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Congratulations, your order has been successfully processed! \
-        Your order reference is {order_number}. A confirmation \
-        email is on its way to {order.email}.')
+    
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+      
+        order.user_profile = profile
+        order.save()
+
+        if save_info:
+            profile_data = {
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_phone_number': order.phone_number,
+                'default_town_or_city': order.town_or_city,
+                'default_county': order.county,
+                'default_postcode': order.postcode,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
@@ -84,5 +123,5 @@ def checkout_success(request, order_number):
     context = {
         'order': order,
     }
-    print(f"Order {order_number} processed successfully")
+
     return render(request, template, context)
