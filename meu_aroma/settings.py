37aaf69a63580b94a7df4aeb3b49fd59257faa7e
zanettiprado <zanettiprado@gmail.com>
2024-01-08 21:09:27 +0000

    firs try of deployment

--- meu_aroma/settings.py
+++ meu_aroma/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 if os.path.exists("env.py"):
   import env 
+import dj_database_url
 import stripe
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-xqv%0w+&p^2k5pi6$d8*oil#!)4by^&7*6=l5_4k_%)%so9m$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-zanettiprado-meuaroma-3zuul62szts.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-zanettiprado-meuaroma-3zuul62szts.ws-eu107.gitpod.io', 'meuaroma.herokuapp.com', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-zanettiprado-meuaroma-3zuul62szts.ws-eu107.gitpod.io']
 
@@ -122,12 +123,19 @@ WSGI_APPLICATION = 'meu_aroma.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
