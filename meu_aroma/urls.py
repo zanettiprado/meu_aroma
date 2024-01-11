757411939eb03ed0d98e3c0e72921e1ced6c01a2
zanettiprado <zanettiprado@gmail.com>
2024-01-11 20:24:11 +0000

    create 404. error Page

--- meu_aroma/urls.py
+++ meu_aroma/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
+from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('products/', include(('products.urls', 'products'), namespace='products')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'meu_aroma.views.handler404'
